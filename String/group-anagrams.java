3292f03136c41b4f7acbcb7b42c6c187ea2fed43
Tarun27 <tarunkr27@gmail.com>
2022-02-27 16:34:13 +0530

    Create group-anagrams.java

---
+++ String/group-anagrams.java
@@ -0,0 +1,94 @@
+//https://leetcode.com/problems/group-anagrams/
+
+class Solution {
+    
+  // sorting solution 
+       
+        Map<String, List<String>> mp= new HashMap();
+        
+        for(String s: strs){
+            
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = String.valueOf(arr);
+                
+            if(!mp.containsKey(key)){
+                mp.put(key,new LinkedList());
+            }
+            
+            mp.get(key).add(s);
+            
+            
+        }
+        
+         return new LinkedList<>(mp.values());
+       
+    
+  
+  
+  /*
+  brute force solution
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        List<List<String>> grpA  = new LinkedList();
+        boolean flag[] = new boolean[strs.length];
+        
+        
+        for(int  i =0;i<strs.length;i++){
+            
+            if(!flag[i]){
+            
+            List<String> ana = new LinkedList();
+            ana.add(strs[i]);
+            
+            for(int j =i+1;j<strs.length;j++){
+                
+               if(!flag[j] && isAnagram(strs[i],strs[j]) ){
+                   ana.add(strs[j]);
+                   flag[j]=true;
+               }
+            }
+            
+            grpA.add(ana);
+            
+            }
+            
+        }
+        
+        return grpA;
+        
+    }
+    
+ public boolean isAnagram(String s, String t) {
+        
+          char arr[] = new char[26];
+          int l = s.length();
+          int m = t.length();
+    
+        if(l!=m){
+            return false;
+        }
+        
+        for(int i=0;i<l;i++){
+            arr[s.charAt(i)-97]++;
+        }
+        
+    
+        for(int i=0;i<m;i++){
+            
+            int x=t.charAt(i)-97;
+            
+            if(arr[x]==0){
+             return false;    
+            }
+            
+            arr[x]--;
+            
+        }
+        
+        return true;
+        
+    }
+    
+    */
+}
