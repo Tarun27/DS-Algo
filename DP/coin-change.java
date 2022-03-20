7f3962194258987aa595018ab7269c899c646958
Tarun27 <tarunkr27@gmail.com>
2022-03-21 02:23:12 +0530

    Update coin-change.java

--- DP/coin-change.java
+++ DP/coin-change.java
@@ -1,5 +1,55 @@
 //https://leetcode.com/problems/coin-change/
 
+// solution 1
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        
+        if(amount==0){
+            return 0;
+         }
+        
+        int memo[]= new int[amount+1];
+      //  int coinsList[]= new int[amount+1];
+      //  Arrays.fill(coinsList,-1);
+        Arrays.fill(memo,99999);
+            memo[0]=0;
+       
+     
+        for(int i=0;i<coins.length;i++){
+            
+            for(int j=1;j<=amount;j++){
+                         
+                if(j>=coins[i]){
+                    memo[j]=Math.min(memo[j],1+memo[j-coins[i]]);     
+                    coinsList[j]=i;
+                } 
+            }
+            
+        }
+        
+    //    printCoins(coins,coinsList);
+
+        return (memo[amount]==99999)? -1 : memo[amount];
+    }
+    
+    public void printCoins(int[] coins, int coinsList[]){
+             int   start = coinsList.length-1;
+        if(coinsList[start]==-1){
+            System.out.println("no solution possible");
+            return;
+        }
+        
+        System.out.println("coins used:   ");
+        
+        while(start>0){
+          System.out.print(coins[coinsList[start]] + "  ");
+            start = start-coins[coinsList[start]];
+        }
+    }
+    
+}
+
+/*  solution 2
 class Solution {
     public int coinChange(int[] coins, int amount) {
         
@@ -31,3 +81,4 @@ class Solution {
         return (memo[coins.length][amount]==99999)? -1 : memo[coins.length][amount];
     }
 }
+*/
