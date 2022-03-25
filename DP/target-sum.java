ccf90922116392d09939ecc8a2fad3a27b44e048
Tarun27 <tarunkr27@gmail.com>
2022-03-26 03:15:35 +0530

    Update target-sum.java

--- DP/target-sum.java
+++ DP/target-sum.java
@@ -3,7 +3,41 @@
 
 
 
+//memo + recursion
 
+class Solution {
+
+    public int findTargetSumWays(int[] nums, int target) {
+        int total = Arrays.stream(nums).sum();
+        //-sum --> 0 --> +sum  
+        int[][] memo = new int[nums.length][2 * total + 1];
+        for (int[] row : memo) {
+            Arrays.fill(row, Integer.MIN_VALUE);
+        }
+        
+        return calculate(nums,target,0,0,memo,total);
+       
+    }
+    
+    public int calculate(int[] nums,int target,int sum, int i, int memo[][], int total){
+        
+        if(i==nums.length){
+        
+        if(sum==target){
+            return 1;
+        }
+            return 0;
+        }else{
+            
+            if (memo[i][sum + total] != Integer.MIN_VALUE) {
+                return memo[i][sum + total];
+            }
+      
+   memo[i][sum+total]= calculate(nums,target,sum+nums[i],i+1,memo,total) +calculate(nums,target,sum-nums[i],i+1,memo,total);
+            return   memo[i][sum+total];
+        }
+    }
+}
 
 //recursive solution
 class Solution {
