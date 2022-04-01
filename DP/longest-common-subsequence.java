f887ac9d0d0cfa94023e3c654a553fdd2d445d87
Tarun27 <tarunkr27@gmail.com>
2022-04-01 09:34:35 +0530

    Update longest-common-subsequence.java
    added get sequence characters

--- DP/longest-common-subsequence.java
+++ DP/longest-common-subsequence.java
@@ -19,6 +19,34 @@ class Solution {
         }
             // System.out.println();
         }
+          getSequenceCharacters(memo,text1,text2);
+        
             return memo[text1.length()][text2.length()];
     }
+    
+        
+public void getSequenceCharacters(int[][] memo, String text1, String text2){
+    StringBuilder sb = new StringBuilder();
+    int i=text1.length(); 
+    int j=text2.length();
+    
+    while(i>0&&j>0){ 
+        
+        if(memo[i][j]==memo[i-1][j-1]+1){
+              i--;
+              j--;
+            sb.append(text1.charAt(i));
+          
+        }
+         else if (memo[i][j] == memo[i-1][j]){
+            i--;
+        } else{
+            j--;
+      }
+    }
+    
+    System.out.println(sb.reverse().toString());
+    
+}
+    
 }
