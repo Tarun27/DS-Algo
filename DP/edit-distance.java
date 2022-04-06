389e9286319561f7e89d3fa6f1c4d2c6d934e0d2
Tarun27 <tarunkr27@gmail.com>
2022-04-07 01:49:42 +0530

    Create edit-distance.java

---
+++ DP/edit-distance.java
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/edit-distance
+
+class Solution {
+    public int minDistance(String word1, String word2) {
+        
+        int memo[][]=new int[word1.length()+1][word2.length()+1];
+        
+        for(int i=0;i<=word1.length();i++){
+            memo[i][0]=i;
+        }
+         for(int i=0;i<=word2.length();i++){
+            memo[0][i]=i;
+        }
+        
+        
+        for(int i=1;i<=word1.length();i++){
+            for(int j=1;j<=word2.length();j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    memo[i][j]=memo[i-1][j-1];
+                }else{
+                    memo[i][j]=Math.min(Math.min(memo[i][j-1],memo[i-1][j]),memo[i-1][j-1])+1;
+                }
+            }
+        }
+        
+        return memo[word1.length()][word2.length()];
+    }
+    
+
+}
