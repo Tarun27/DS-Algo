f177ab18b3c765e4378810cf32b857ef1b60c9cc
Tarun27 <tarunkr27@gmail.com>
2022-04-26 04:05:02 +0530

    Update longest-increasing-subsequence.java

--- DP/longest-increasing-subsequence.java
+++ DP/longest-increasing-subsequence.java
@@ -6,37 +6,36 @@
 // O(n2) square solution
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        
-        int lis=1;
-        int memo[] = new int[nums.length];
-        memo[0]=1;
-        for(int i=0;i<nums.length;i++){
-        
-     lis = Math.max(lis,calcLis(i,nums,memo));
-         
-        }   
-        
-        return lis;
-    }
-
-    int calcLis(int i,int[] nums, int memo[]){
-        
-        if(i==0){
+    
+        if(nums.length<=1){
             return 1;
         }
-    
-      int  maxLis=1;
         
-        for(int j=0;j<i;j++){
+        int memo[]= new int[nums.length];
+        int max=1;
+        
+        
+        Arrays.fill(memo,1);
+        
+        for(int i=1;i<nums.length;i++){
             
-            if(nums[i]>nums[j]){
-            maxLis = Math.max(maxLis,memo[j]+1);
+            int j=0;
+            
+            while(i>j){
+                if(nums[i]>nums[j]){
+                memo[i]=Math.max(memo[i],memo[j]+1);
+                }
+                if(memo[i]>max){
+                    max=memo[i];
+                }
+                j++;
+                if(j>=nums.length){break;}
             }
+            
         }
-        memo[i]=maxLis;
-        return maxLis;
-    }
+   return max;
     
+}
 }
 
 
