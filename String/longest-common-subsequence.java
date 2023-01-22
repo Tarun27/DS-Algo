22489276078d721da63a1491ce678184321af2b8
Tarun27 <tarunkr27@gmail.com>
2023-01-22 22:23:10 +0530

    Update longest-common-subsequence.java

--- String/longest-common-subsequence.java
+++ String/longest-common-subsequence.java
@@ -1,6 +1,25 @@
 //https://leetcode.com/problems/longest-common-subsequence/
 
 class Solution {
+    
+ //sol 1 recursive
+     public int longestCommonSubsequence(String s1, String s2) {
+        
+        return lcs(s1, s2,0,0);
+
+    }
+
+    public int lcs(String s1, String s2, int i1, int i2){
+
+        if(i1>=s1.length() || i2>=s2.length())return 0;
+
+        if(s1.charAt(i1)==s2.charAt(i2)) return 1 + lcs(s1,s2,i1+1,i2+1);
+        else return Math.max(lcs(s1,s2,i1+1,i2),lcs(s1,s2,i1,i2+1));
+
+    }
+    
+  // sol2   iterative
+ 
     public int longestCommonSubsequence(String text1, String text2) {
         
         int memo[][] = new int[text1.length()+1][text2.length()+1];
