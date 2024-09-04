cef0af4058f64d6ba904ef0622f59945fd95e412
Tarun27 <tarunkr27@gmail.com>
2024-09-05 00:40:28 +0530

    Update permutations.java

--- Backtracking/permutations.java
+++ Backtracking/permutations.java
@@ -30,4 +30,24 @@ class Solution {
         }
         
     }
+
+// another way to do this
+
+
+       private static void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] nums) {
+        if (tempList.size() == nums.length) {
+            result.add(new ArrayList<>(tempList));
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if (!tempList.contains(nums[i])){
+                    tempList.add(nums[i]);
+                    backtrack(result, tempList, nums);
+                    tempList.remove(tempList.size() - 1);
+                    
+                } // Skip duplicates
+              
+            }
+        }
+    }
+    
 }
